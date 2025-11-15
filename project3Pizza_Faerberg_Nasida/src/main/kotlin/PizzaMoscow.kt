cbb65dea150800124069b05fb9fdcd1d8857c3b1
YarfolwLn <markfaerberg@gmail.com>
2025-11-15 11:15:53 +0300

    Update PizzaMoscow.kt

--- project3Pizza_Faerberg_Nasida/src/main/kotlin/PizzaMoscow.kt
+++ project3Pizza_Faerberg_Nasida/src/main/kotlin/PizzaMoscow.kt
@@ -6,26 +6,64 @@ class PizzaMoscow(
     sicilianPizzaPrice, tyroleanPizzaPrice
 ), CheckPhoto {
 
+    override var totalDiscount = 0.0
+    override var checkShownCount = 0
+    override var checkNotShownCount = 0
+
     override fun showCheckPhoto() {
         println("У вас есть фотография чека?")
         println("1. Да\n2. Нет")
-        if (readln() == "1") println("Вам будет скидка 50 рублей с покупки")
+        when (readln()) {
+            "1" -> {
+                println("Вам будет скидка 50 рублей с покупки")
+                totalDiscount += 50.0
+                checkShownCount++
+            }
+            "2" -> {
+                checkNotShownCount++
+            }
+        }
     }
 
     override fun neapolitanPizzaSale() {
         neapolitanPizzaCount++
         println("Спасибо за покупку неаполитанской пиццы в Москве")
     }
+
     override fun romanPizzaSale() {
         romanPizzaCount++
         println("Спасибо за покупку римской пиццы в Москве")
     }
+
     override fun sicilianPizzaSale() {
         sicilianPizzaCount++
         println("Спасибо за покупку сицилийской пиццы в Москве")
     }
+
     override fun tyroleanPizzaSale() {
         tyroleanPizzaCount++
         println("Спасибо за покупку тирольской пиццы в Москве")
+    }
+
+    override fun showStatistics() {
+        println("=== Статистика по пиццерии Москва ===")
+        println(getPizzaStats())
+
+        val pizzaRevenue = getTotalPizzaRevenue()
+        val totalRevenue = pizzaRevenue - totalDiscount
+
+        println("Выручка за пиццу: $pizzaRevenue")
+        println("Общая сумма скидок: $totalDiscount")
+        println("Итоговая выручка (с учетом скидок): $totalRevenue")
+
+        // Статистика по чекам
+        val totalChecks = checkShownCount + checkNotShownCount
+        if (totalChecks > 0) {
+            val shownPercentage = (checkShownCount.toDouble() / totalChecks) * 100
+            val notShownPercentage = (checkNotShownCount.toDouble() / totalChecks) * 100
+            println("\nСтатистика по чекам:")
+            println("Показали чек: $checkShownCount (${"%.2f".format(shownPercentage)}%)")
+            println("Не показали чек: $checkNotShownCount (${"%.2f".format(notShownPercentage)}%)")
+        }
     }
 }
