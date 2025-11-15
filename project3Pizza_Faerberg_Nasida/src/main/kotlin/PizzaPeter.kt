5e0e0c333f27bfb0a9ed030da504b9fe3f30a77a
YarfolwLn <markfaerberg@gmail.com>
2025-11-15 11:15:38 +0300

    Update PizzaPeter.kt

--- project3Pizza_Faerberg_Nasida/src/main/kotlin/PizzaPeter.kt
+++ project3Pizza_Faerberg_Nasida/src/main/kotlin/PizzaPeter.kt
@@ -6,26 +6,91 @@ class PizzaPeter(
     sicilianPizzaPrice, tyroleanPizzaPrice
 ), Drink {
 
+    override var coffeeSoldCount = 0
+    override var coffeeRevenue = 0.0
+    override var coffeeRefusedCount = 0
+    override val pizzaWithCoffee = mutableMapOf(
+        "Неаполитанская" to 0,
+        "Римская" to 0,
+        "Сицилийская" to 0,
+        "Тирольская" to 0
+    )
+
+    private var currentPizzaType = ""
+
     override fun drinkSale() {
         println("Вы будете кофе?")
         println("1. Да\n2. Нет")
-        if (readln() == "1") println("С вас 200 рублей")
+        when (readln()) {
+            "1" -> {
+                println("С вас 200 рублей за кофе")
+                coffeeSoldCount++
+                coffeeRevenue += 200.0
+                // Увеличиваем счетчик кофе для текущего типа пиццы
+                pizzaWithCoffee[currentPizzaType] = pizzaWithCoffee[currentPizzaType]!! + 1
+            }
+            "2" -> {
+                coffeeRefusedCount++
+            }
+        }
     }
 
     override fun neapolitanPizzaSale() {
         neapolitanPizzaCount++
+        currentPizzaType = "Неаполитанская"
         println("Спасибо за покупку неаполитанской пиццы в Санкт-Петербурге")
     }
+
     override fun romanPizzaSale() {
         romanPizzaCount++
+        currentPizzaType = "Римская"
         println("Спасибо за покупку римской пиццы в Санкт-Петербурге")
     }
+
     override fun sicilianPizzaSale() {
         sicilianPizzaCount++
+        currentPizzaType = "Сицилийская"
         println("Спасибо за покупку сицилийской пиццы в Санкт-Петербурге")
     }
+
     override fun tyroleanPizzaSale() {
         tyroleanPizzaCount++
+        currentPizzaType = "Тирольская"
         println("Спасибо за покупку тирольской пиццы в Санкт-Петербурге")
+    }
+
+    override fun showStatistics() {
+        println("=== Статистика по пиццерии Санкт-Петербург ===")
+        println(getPizzaStats())
+
+        val pizzaRevenue = getTotalPizzaRevenue()
+        val totalRevenue = pizzaRevenue + coffeeRevenue
+
+        println("Выручка за пиццу: $pizzaRevenue")
+        println("Продано кофе: $coffeeSoldCount")
+        println("Выручка за кофе: $coffeeRevenue")
+        println("Итоговая выручка: $totalRevenue")
+
+        // Статистика по кофе
+        val totalCoffeeOffers = coffeeSoldCount + coffeeRefusedCount
+        if (totalCoffeeOffers > 0) {
+            val boughtPercentage = (coffeeSoldCount.toDouble() / totalCoffeeOffers) * 100
+            val refusedPercentage = (coffeeRefusedCount.toDouble() / totalCoffeeOffers) * 100
+            println("\nСтатистика по кофе:")
+            println("Купили кофе: $coffeeSoldCount (${"%.2f".format(boughtPercentage)}%)")
+            println("Отказались от кофе: $coffeeRefusedCount (${"%.2f".format(refusedPercentage)}%)")
+
+            // Статистика по пиццам с кофе
+            println("\nСтатистика по пиццам с кофе:")
+            val totalCoffeeWithPizza = coffeeSoldCount
+            if (totalCoffeeWithPizza > 0) {
+                pizzaWithCoffee.forEach { (pizzaType, count) ->
+                    if (count > 0) {
+                        val percentage = (count.toDouble() / totalCoffeeWithPizza) * 100
+                        println("$pizzaType: $count (${"%.2f".format(percentage)}%)")
+                    }
+                }
+            }
+        }
     }
 }
