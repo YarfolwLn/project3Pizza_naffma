167aecec91d306c98fd28a0278d0a65f199741da
YarfolwLn <markfaerberg@gmail.com>
2025-11-15 11:14:55 +0300

    Update Drink.kt

--- project3Pizza_Faerberg_Nasida/src/main/kotlin/Drink.kt
+++ project3Pizza_Faerberg_Nasida/src/main/kotlin/Drink.kt
@@ -1,4 +1,7 @@
 interface Drink {
-
     fun drinkSale()
+    val coffeeSoldCount: Int
+    val coffeeRevenue: Double
+    val coffeeRefusedCount: Int
+    val pizzaWithCoffee: MutableMap<String, Int>
 }
